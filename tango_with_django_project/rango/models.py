1141bd026c7e427fa2fd9f4bc349739a6063d92e
NuraAbdul-Rahman <2418644a@student.gla.ac.uk>
2019-01-31 16:09:03 +0000

    Chapter 6:Models,Templates & Views

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,25 +1,28 @@
 from django.db import models
-from django.contrib import admin
-
+from django.template.defaultfilters import slugify
 class Category(models.Model):
     name=models.CharField(max_length=128, unique=True)
     views=models.IntegerField(default=0)
     likes=models.IntegerField(default=0)
+    slug=models.SlugField(unique=True)
+
+    def save(self,*args,**kwargs):
+        self.slug=slugify(self.name)
+        super(Category,self).save(*args,**kwargs)
 
     class Meta:
-        verbose_name_plural='Categories'
+        verbose_name_plural='categories'
 
     def __str__(self):
         return self.name
+        
 
 class Page(models.Model):
     category=models.ForeignKey(Category)
     title=models.CharField(max_length=128)
     url=models.URLField()
     views=models.IntegerField(default=0)
-
-class PageAdmin(admin.ModelAdmin):
-    list_display=('title','category','url')
+    #slug=models.SlugField(unique=True)
 
     def __str__(self):
         return self.title
